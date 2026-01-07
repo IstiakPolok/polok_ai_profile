8b8700d66041981157dfd6f792e4b6e0dca8fc1e
Fatin Istiak Polok <fatin15-1944@diu.edu.bd>
2026-01-07 11:04:52 +0600

    fdsfsd

--- build.sh
+++ build.sh
@@ -1,23 +1,25 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
 # Install Flutter if not present
 if [ ! -d "_flutter" ]; then
+  echo "Cloning Flutter..."
   git clone https://github.com/flutter/flutter.git --depth 1 -b stable _flutter
 fi
 
 # Use absolute path for Flutter
 export PATH="$PWD/_flutter/bin:$PATH"
 
-# Print environment for debugging
-printenv
-
-# Verify Flutter installation
-flutter --version
-flutter doctor
-
 # Enable web support
+echo "Enabling web support..."
 flutter config --enable-web
 
 # Get dependencies
+echo "Getting dependencies..."
 flutter pub get
 
 # Build for web
-flutter build web --release --web-renderer html
+echo "Building for web..."
+flutter build web --release --web-renderer html --base-href /
